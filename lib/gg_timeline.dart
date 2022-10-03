18fac5a9e850545a3f5bd3f7aacfb3b98217c0b2
Gabriel Gatzsche <gabriel.gatzsche@grace.cloud>
2022-10-03 09:48:08 +0200

    Clean dart package

---
+++ lib/gg_timeline.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library gg_timeline;
+
+export 'src/gg_timeline_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
